f22e512dcb28b1f18554912affc8d1f20abf06ad
Josh <joshua.hansen188@gmail.com>
2019-06-14 18:01:25 +1000

    started adding volume and battery to the bar

--- statusbar.sh
+++ statusbar.sh
@@ -4,6 +4,16 @@ delim=" | "
 
 function status() {
     echo $delim
+    
+   
+    echo "$(cat /sys/class/power_supply/BAT1/capacity)%"
+    
+    echo $delim
+
+    amixer get Master | awk -F'[][]' 'END{ print $2 }'
+
+    echo $delim
+
     date "+%d/%m/%Y %I:%M"
 }
 
@@ -11,5 +21,5 @@ while :; do
     xsetroot -name "$(status | tr '\n' ' ')"
 
     # Sleep for 1 minute before updating the status bar
-    sleep 10s
+    sleep 1m
 done
