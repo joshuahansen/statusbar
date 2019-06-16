b1d0fd5ae2d8975f1c1e7cd0d3f338cb6909b9db
Josh <joshua.hansen188@gmail.com>
2019-06-16 12:27:25 +1000

    updated status bar

--- statusbar.sh
+++ statusbar.sh
@@ -2,20 +2,58 @@
 
 delim=" | "
 
+function battery() {
+    bat=$(< /sys/class/power_supply/BAT1/capacity)
+    
+    if (($bat < 25));
+    then
+        echo "$bat%" # $(~/suckless-builds/d-icons/battery-0.svg)"
+    elif (($bat < 50));
+    then
+        echo "$bat%" # $(~/suckless-builds/d-icons/battery-1.svg)"
+    elif (($bat < 75));
+    then
+        echo "$bat%" # $(~/suckless-builds/d-icons/battery-2.svg)"
+    else
+        echo "$bat%" # $(~/suckless-builds/d-icons/battery-3.svg)"
+    fi
+}
+
+function volume() {
+    vol=$(amixer -M get Master | awk -F'[][]' 'END{ print $2 }')
+
+    if (($vol == 0));
+    then
+        echo "mute"
+    elif (($vol < 25));
+    then
+        echo "$vol"
+    elif (($vol < 50));
+    then
+        echo "$vol"
+    elif (($vol < 75));
+    then
+        echo "$vol"
+    else
+        echo "$vol"
+    fi
+}
+
 function status() {
     echo $delim
     
-   
-    echo "$(cat /sys/class/power_supply/BAT1/capacity)%"
-    
+    echo $(battery)
+
     echo $delim
 
-    amixer -M get Master | awk -F'[][]' 'END{ print $2 }'
+    vol=$(amixer -M get Master | awk -F'[][]' 'END{ print $2 }')
+    echo $vol
 
     echo $delim
 
     date "+%d/%m/%Y %I:%M"
 }
+
 
 while :; do
     xsetroot -name "$(status | tr '\n' ' ')"
