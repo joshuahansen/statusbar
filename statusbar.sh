5e575320866e0660fd25b7c31e4bce7729a8b108
Josh <joshua.hansen188@gmail.com>
2020-08-08 16:28:54 +1000

    Updated statusbar
    Added support for font awesome.
    Removed vertial delimiters.

--- statusbar.sh
+++ statusbar.sh
@@ -1,50 +1,61 @@
-#!/bin/bash
+#/bin/bash
 
 # Red colour
 RED="^c#FF0000^"
 # Green colour
-GREEN="^00FF00^"
+GREEN="^c#00FF00^"
 # No Colour
 NC="^d^"
 # Delimiter
-DELIM=" | "
+DELIM=" "
 
 LOW_BAT=false
 
 function battery() {
     bat=$(< /sys/class/power_supply/BAT1/capacity)
-    
+
     if (($bat < 25)); then
         if (( $bat <= 10 )) && [ "$LOW_BAT" = false ]; then
             LOW_BAT=true
+            echo -e "\uf244 $bat%"
             notify-send -u critical "Low Battery" "Battery is $bat% plug into charger"
+        else
+            LOW_BAT=false
+            echo -e "\uf243 $bat%"
         fi
-        LOW_BAT=false
-        echo "$bat%" # $(~/suckless-builds/d-icons/battery-0.svg)"
     elif (($bat < 50)); then
         LOW_BAT=false
-        echo "$bat%" # $(~/suckless-builds/d-icons/battery-1.svg)"
+        echo -e "\uf242 $bat%"
     elif (($bat < 75)); then
         LOW_BAT=false
-        echo "$bat%" # $(~/suckless-builds/d-icons/battery-2.svg)"
+        echo -e "\uf241 $bat%"
     else
         LOW_BAT=false
-        echo "$bat%" # $(~/suckless-builds/d-icons/battery-3.svg)"
+        echo -e "\uf240 $bat%"
     fi
 }
 
 function volume() {
-    vol=$(amixer -M get Master | awk -F'[][]' 'END{ print $2 }')
-    if ((${vol::-1} == 0)); then
-        echo "mute"
-    elif ((${vol::-1} < 25)); then
-        echo "$vol"
-    elif ((${vol::-1} < 50)); then
-        echo "$vol"
-    elif ((${vol::-1} < 75)); then
-        echo "$vol"
+    vol=$(pamixer --get-volume)
+    mute=$(pamixer --get-mute)
+
+    if [ "$mute" == "true" ]; then
+        echo -e "\uf6a9"
+    elif (($vol < 25)); then
+        echo -e "\uf026 $vol%"
+    elif (($vol < 50)); then
+        echo -e "\uf027 $vol%"
     else
-        echo "$vol"
+        echo -e "\uf028 $vol%"
+    fi
+}
+
+function vpn() {
+    vpnStatus=$(nordvpn status | grep -q "Disconnected" ; echo $?)
+    if [[ $vpnStatus -eq 0 ]]; then
+        echo -e "\uf3c1"
+    else
+        echo -e "\uf023"
     fi
 }
 
@@ -55,11 +66,12 @@ function wifi() {
     else
         wifistatus=$(cat /sys/class/net/wlp4s0/operstate)
         if [[ "$wifistatus" == "up" ]]; then
-            echo "wifi on"
+            echo -e "\uf1eb"
         fi
     fi
+    vpn
     ip=$(wget http://ipecho.net/plain -O - -q)
-    echo  "$RED$ip$NC"
+    echo "$ip"
 }
 
 function weather() {
@@ -71,11 +83,12 @@ function weather() {
     echo "$(head -n 2 wttr.txt | grep C | sed -e 's/C/C /g')"
 }
 
+function bluetooth() {
+    echo -e "\uf294"
+}
+
+
 function status() {
-
-    echo $DELIM
-
-    weather
 
     echo $DELIM
 
@@ -91,7 +104,9 @@ function status() {
 
     echo $DELIM
 
-    date "+%d/%m/%Y %I:%M"
+    date "+%d/%m/%Y %H:%M"
+
+    echo -e "\uf17c"
 }
 
 while :; do
